4d97b2ffa75f84642daa6f1a921319bc75667cb9
Hoang-Son NGUYEN <son@MBPdeHoangSon.lan>
2022-11-21 23:54:04 +0100

    test

---
+++ partie1.pl
@@ -0,0 +1,43 @@
+equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(auteur,and(personne,some(aEcrit,livre))).
+equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite
+,livre)))).
+equiv(parent,and(personne,some(aEnfant,anything))).
+cnamea(personne).
+cnamea(livre).
+cnamea(objet).
+cnamea(sculpture).
+cnamea(anything).
+cnamea(nothing).
+cnamena(auteur).
+cnamena(editeur).
+cnamena(sculpteur).
+cnamena(parent).
+iname(michelAnge).
+iname(david).
+iname(sonnets).
+iname(vinci).
+iname(joconde).
+rname(aCree).
+rname(aEcrit).
+rname(aEdite).
+rname(aEnfant).
+inst(michelAnge,personne).
+inst(david,sculpture).
+inst(sonnets,livre).
+inst(vinci,personne).
+inst(joconde,objet).
+instR(michelAnge, david, aCree).
+instR(michelAnge, sonnets, aEcrit).
+instR(vinci, joconde, aCree).
+tbox([(sculpteur,and(personne,some(aCree,sculpture))),
+(auteur,and(personne,some(aEcrit,livre))),
+(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))),
+(parent,and(personne,some(aEnfant,anything)))]).
+abox([(michelAnge,personne), (david,sculpture), (sonnets,livre),
+(vinci,personne), (joconde,objet)]).
+assert_role([(michelAnge, david, aCree), (michelAnge, sonnet, aEcrit),(vinci,
+joconde, aCree)]).
+
+%Correction sémantique
+testrname(X):- setof(Y, rname(Y), L), member(X,L).
