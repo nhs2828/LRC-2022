aaed6cb49f03eba1dbd16e66bbb3a76bff45e315
Valinquish <13287345+Valinquish@users.noreply.github.com>
2022-12-10 22:03:57 +0100

    start partie3
    tri_Abox

--- partie2.pl
+++ partie2.pl
@@ -12,13 +12,15 @@ saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox) :-
 suite(1,Abi,Abi1,Tbox) :- acquisition_prop_type1(Abi,Abi1,Tbox),!.
 suite(2,Abi,Abi1,Tbox) :- acquisition_prop_type2(Abi,Abi1,Tbox),!.
 suite(R,Abi,Abi1,Tbox) :- 
-  nl,write('Cette réponse est inccorecte.'),
+  nl,write('Cette réponse est incorrecte.'),
   nl,saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
 
 %acquisition_prop_type1
 acquisition_prop_type1([],[],_).
-acquisition_prop_type1([X|Abi],[Y|Abi1],Tbox):-traitement_Abox(not(X),Y,Tbox), acquisition_prop_type1(Abi,Abi1,Tbox).
+acquisition_prop_type1([X|Abi],[Y|Abi1],Tbox) :- 
+  traitement_Abox(not(X),Y,Tbox), acquisition_prop_type1(Abi,Abi1,Tbox).
 
 %acquisition_prop_type2
 acquisition_prop_type2([],[],_).
-acquisition_prop_type2([and(X1,X2)|Abi],[Y|Abi1],Tbox):-traitement_Abox(and(X1,X2),Y,Tbox), acquisition_prop_type2(Abi,Abi1,Tbox).
+acquisition_prop_type2([and(X1,X2)|Abi],[Y|Abi1],Tbox) :- 
+  traitement_Abox(and(X1,X2),Y,Tbox), acquisition_prop_type2(Abi,Abi1,Tbox).
--- partie3.pl
+++ partie3.pl
@@ -2,3 +2,25 @@ troisieme_etape(Abi,Abr) :-
   tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
   resolution(Lie,Lpt,Li,Lu,Ls,Abr),
   nl,write('Youpiiiiii, on a demontre la proposition initiale !!!').
+
+% Tri les assertions de la Abox étendue
+tri_Abox([],[],[],[],[],[]).
+% Lie <- Assertions de type (I,some(R,C))
+tri_Abox([(I,some(R,C))|Abi],[(I,some(R,C))|Lie],Lpt,Li,Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+% Lpt <- Assertions de type (I,all(R,C))
+tri_Abox([(I,all(R,C))|Abi],Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+% Li <- Assertions de type (I,and(C1,C2))
+tri_Abox([(I,and(C1,C2))|Abi],Lie,Lpt,[(I,and(C1,C2))|Li],Lu,Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+% Lu <- Assertions de type (I,or(C1,C2))
+tri_Abox([(I,or(C1,C2))|Abi],Lie,Lpt,Li,[(I,or(C1,C2))|Lu],Ls) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls).
+% Ls <- Assertions de type (I,C)
+tri_Abox([(I,C)|Abi],Lie,Lpt,Li,Lu,[(I,C)|Ls]) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),!.
+% Ls <- Assertions de type (I,not(C))
+tri_Abox([(I,not(C))|Abi],Lie,Lpt,Li,Lu,[(I,not(C))|Ls]) :- 
+  tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),!.
+
